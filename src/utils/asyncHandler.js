7faad92d5d3d301bed26fd44dc846196dde9c9db
Yuvraj Singh Gohil <yuvrajgohil1817@gmail.com>
2023-11-25 00:43:00 +0530

    Configuration Setup

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (func) => {
+    (req, res, next) => {
+        Promise.resolve(func(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+//         await func(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
